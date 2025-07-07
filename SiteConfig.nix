065ea3cc0d5db3664abfe441b169085255a57644
Adam Fairbrother <adam.fairbrother@gmail.com>
2025-07-07 14:24:42 -0700

    Broke siteconfig off to allow easier updates of main config

---
+++ SiteConfig.nix
@@ -0,0 +1,9 @@
+{
+  # Adjust these values to suit the site.
+  adDomain = "Sample.Full.Domain.name";
+  adWorkgroup = "WORKGROUP";
+  adNetbiosName = "AD";
+  AdContainerIp = "127.0.0.1";
+  hostServerIp = "127.0.0.2";
+  webServerIp = "127.0.0.3";
+}
